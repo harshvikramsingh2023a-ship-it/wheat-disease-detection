11b0d56599465834083e1a053f67f0d950b91418
harshvikramsingh2023a-ship-it <harshvikram.singh2023a@vitstudent.ac.in>
2026-04-05 22:28:35 +0530

    Update appl.py

--- appl.py
+++ appl.py
@@ -1,35 +1,29 @@
 import streamlit as st
 import numpy as np
-import tflite_runtime.interpreter as tflite
+from tensorflow.keras.preprocessing import image
+from tensorflow.keras.models import load_model
 from PIL import Image
 
-interpreter = tflite.Interpreter(model_path="model.tflite")
-interpreter.allocate_tensors()
+model = load_model("wheat_model.h5")
 
-input_details = interpreter.get_input_details()
-output_details = interpreter.get_output_details()
+classes = ['Healthy', 'Rust', 'Mildew']
 
-classes = ['Healthy', 'Mildew', 'Rust']
+st.title("🌾 Wheat Disease Detection")
 
-st.title("🌾 Wheat Disease Detection System")
-
-uploaded_file = st.file_uploader("Upload image", type=["jpg", "png", "jpeg"])
+uploaded_file = st.file_uploader("Upload Image", type=["jpg","png","jpeg"])
 
 if uploaded_file:
     img = Image.open(uploaded_file)
     st.image(img, caption="Uploaded Image")
 
-    img = img.resize((224, 224))
-    img_array = np.array(img, dtype=np.float32) / 255.0
+    img = img.resize((224,224))
+    img_array = np.array(img)/255.0
     img_array = np.expand_dims(img_array, axis=0)
 
-    interpreter.set_tensor(input_details[0]['index'], img_array)
-    interpreter.invoke()
-
-    prediction = interpreter.get_tensor(output_details[0]['index'])
+    prediction = model.predict(img_array)
 
     result = classes[np.argmax(prediction)]
-    confidence = np.max(prediction) * 100
+    confidence = np.max(prediction)*100
 
     st.success(f"Prediction: {result}")
     st.info(f"Confidence: {confidence:.2f}%")
